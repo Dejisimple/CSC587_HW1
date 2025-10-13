66f711676eb148f43f4bac79da9f9eefdb75a2ba
Dejisimple <147572513+Dejisimple@users.noreply.github.com>
2025-10-12 22:50:48 -0400

    myfirstRScript
    Scripts

---
+++ myfirstRScript.R
@@ -0,0 +1,36 @@
+---
+title: "Untitled"
+output: pdf
+---
+
+
+```{r}
+su<-read.delim("C:\\Users\\HOME\\Downloads\\Su_raw_matrix.txt")
+
+
+```
+
+
+```
+
+## Including Plots
+
+You can also embed plots, for example:
+  
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+
+
+
+```{r}
+su = rnorm(1000, mean = 0, sd = 2)
+hist(values)
+
+```
+
+
+
